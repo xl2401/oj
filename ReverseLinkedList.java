31bc02884753996e2891e86b9152a1e9bd3ad57d
Yuhan Zhang <njuprincerain@gmail.com>
2014-01-09 15:23:37 -0500

    add comment

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -1,5 +1,17 @@
 import java.util.LinkedList;
 
+/*
+ * Title: Reverse Linked List II 
+ * Description:
+ * Reverse a linked list from position m to n. Do it in-place and in one-pass.
+ *
+ * For example:
+ * Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+ *
+ * return 1->4->3->2->5->NULL.
+ *
+ * Solution: not in place
+ */
 public class ReverseLinkedList {
     public ListNode reverseBetween(ListNode head, int m, int n) {
         ListNode p = head;
