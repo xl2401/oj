906277ec65e9c506cef1a080e62281efa2ed2977
Yuhan Zhang <njuprincerain@gmail.com>
2014-01-13 15:56:32 -0500

    Longest Palindrome Substring

---
+++ LongestPalindrome.java
@@ -0,0 +1,52 @@
+/*
+ * Title: Longest Palindromic Substring 
+ * Description:
+ * Given a string S, find the longest palindromic substring in S.
+ * You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ *
+ * Solution:
+ * The very naive method is to choose two idx as start and end and judge if it is a palindrome,
+ * however this method is O(n^3), so it's very slow.
+ *
+ * Use DP can improve the complexity to O(n^2)
+ * How to break the palindrome problem into smaller problem?
+ * consider str of idx i, j(i < j && j-i>1). if str[i] == str[j] than we need str.substring(i+1, j) to be palindrome
+ * so the len of subproblem is originallen-2, so we can solve shorter string first, from 1-> N
+ * the matrix isPal record if str from i to j is palindrome, if yes, the item will be 1
+ */
+public class LongestPalindrome {
+    public String longestPalindrome(String s) {
+        int N = s.length();
+        int[][] isPal = new int[N][N];
+        int max = 0;
+        int start = 0;
+        int end = 0;
+        for (int l = 1; l <= N; l++){// len of the s
+            for (int i = 0; i < N-l+1; i++){
+                if (s.charAt(i) == s.charAt(i+l-1)){
+                    if (l == 1 || l == 2){
+                        isPal[i][i+l-1] = 1;
+                        if (l > max){
+                            max = l;
+                            start = i;
+                            end = i+l-1;
+                        }
+                    }
+                    else if (isPal[i+1][i+l-2] == 1){
+                        isPal[i][i+l-1] = 1;
+                        if (l > max){
+                            max = l;
+                            start = i;
+                            end = i+l-1;
+                        }
+                    }
+                }
+            }
+        }
+        return s.substring(start, end+1);
+    }
+
+    public static void main(String[] args){
+        System.out.println(new LongestPalindrome().longestPalindrome("caba"));
+    }
+}
