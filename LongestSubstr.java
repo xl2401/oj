dd5c502da594075d6f3c1ee9d0993cb99ae8f193
Yuhan Zhang <njuprincerain@gmail.com>
2014-02-09 16:00:14 -0500

    rewrite longest substring

--- LongestSubstr.java
+++ LongestSubstr.java
@@ -1,25 +1,45 @@
 import java.util.Arrays;
 
-public class LongestSubstr {
-    public int lengthOfLongestSubstring(String s) {
-        int maxLength = 0;
-        int[] occurArray = new int[256];
-        Arrays.fill(occurArray, -1);
+/*
+ * Title: Longest Substring Without Repeating Characters
+ * Description:
+ * Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+ *
+ * Solution:
+ * Here, use an array appearIdx to record the last idx that a character appears, this array is 256 large including all ASCII char
+ * pay attention:
+ * 1. judge if a char is a repetition: the idx should within the range!
+ * 2. the start point should be lastAppear + 1, not ptr
+ * last, do not forget update max outside the loop, because it is possible the last char is not a repetition
+ */
+public class LongestSubstr{
+    public int lengthOfLongestSubstring(String s){
+        int[] appearIdx = new int[256];
+        Arrays.fill(appearIdx, -1);
+        int ptr = 0;
         int start = 0;
-        int pointer = 0;
-        while (pointer < s.length()){
-            char c = s.charAt(pointer);
-            int lastOccurIndex = occurArray[c];
-            if (!(lastOccurIndex >= start && lastOccurIndex < pointer)){
-                occurArray[c] = pointer;
-                pointer++;
+        int max = 0;
+        int N = s.length();
+        while (ptr < N){
+            char c = s.charAt(ptr);
+            int lastAppear = appearIdx[c];
+            if (lastAppear >= start && lastAppear < ptr){ // appear again!
+                // update appearance
+                appearIdx[c] = ptr;
+                max = Math.max(max, ptr - start);
+                start = lastAppear + 1;
             }
-            else{
-                maxLength = Math.max(maxLength, (pointer - start));
-                start = lastOccurIndex + 1;
-                pointer = start;
+            else{// simply update appearance
+                appearIdx[c] = ptr;
             }
+            ptr++;
         }
-        return Math.max(maxLength, (pointer - start));
+        max = Math.max(max, N - start);
+        return max;
+    }
+
+    public static void main(String[] args){
+        LongestSubstr lss = new LongestSubstr();
+        System.out.println(lss.lengthOfLongestSubstring("wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco"));
     }
 }
