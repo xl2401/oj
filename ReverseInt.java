5cedb2af28fac0e289ffa10c9acf0e2121c06931
Yuhan Zhang <njuprincerain@gmail.com>
2014-02-09 21:08:15 -0500

    rewrite reverse int with overflow check

--- ReverseInt.java
+++ ReverseInt.java
@@ -1,21 +1,38 @@
-public class ReverseInt {
-    public int reverse(int x) {
-        boolean positive = true;
+/*
+ * Title: Reverse Integer 
+ * Description:
+ * Reverse digits of an integer.
+ *
+ * Example1: x = 123, return 321
+ * Example2: x = -123, return -321
+ *
+ * Solution:
+ * use mod to retrive last digit first and construct a reverse int
+ * Check overflow is tricky
+ */
+public class ReverseInt{
+    public int reverse(int x) throws Exception{
+        int sign = 1;
         if (x < 0){
-            positive = false;
-            x = -x;
+            sign = -1;
+            x *= -1;
         }
         int result = 0;
-        while (true){
-            int remainder = x % 10;
-            x = x/10;
-            result *= 10;
-            result += remainder;
-            if (x == 0) break;
+        while (x != 0){
+            if (result > Integer.MAX_VALUE / 10){
+                // multi by 10 will overflow
+                throw new Exception("OVERFLOW");
+            }
+            else{
+                result *= 10;
+            }
+            result += x % 10;
+            if (result < 0){
+                throw new Exception("OVERFLOW");
+            }
+            x /= 10;
         }
-        if (positive == false){
-            result = -1 * result;
-        }
+        result *= sign;
         return result;
     }
 }
