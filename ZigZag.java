d4ccf2bdbefe5b7e990d808caf03f7269eff861a
Yuhan Zhang <njuprincerain@gmail.com>
2014-02-09 20:51:09 -0500

    rewrite ZigZag

--- ZigZag.java
+++ ZigZag.java
@@ -1,37 +1,45 @@
-public class ZigZag {
-    public String convert(String s, int nRows) {
+/*
+ * Title: ZigZag Conversion
+ * Solution:
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ *
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * Write the code that will take a string and make this conversion given a number of rows:
+ *
+ * string convert(string text, int nRows);
+ * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+ *
+ * Solution:
+ * Create nRows StringBuilder represent each row
+ * iterate over the string, revise row num in zigzag fashion, add character into corresponding row stringbuilder
+ * merge all row into a single string
+ * # pay attention that when nRows == 1, this does not suite. So this is a special condition
+ */
+public class ZigZag{
+    public String convert(String s, int nRows){
         if (nRows == 1) return s;
-        StringBuilder sb = new StringBuilder();
-        int N = s.length();
-        
-        int add1 = 2 * nRows - 4;
-        int add2 = 2;
+        StringBuilder[] sbRow = new StringBuilder[nRows];
         for (int i = 0; i < nRows; i++){
-            if (i == 0 || i == (nRows-1)){ // First row or last row
-                int pointer = i;
-                while (true){
-                    if (pointer < N){
-                        sb.append(s.charAt(pointer));
-                        pointer += 2 * nRows - 2;
-                    }
-                    else break;
-                }
+            sbRow[i] = new StringBuilder();
+        }
+        int row = 0;
+        int head = 1;        
+        for (int i = 0; i < s.length(); i++){
+            if (row == 0){
+                head = 1;
             }
-            else{ // Middle zig zag
-                int pointer = i;
-                int j = 0;
-                while (true){
-                    if (pointer < N){
-                        sb.append(s.charAt(pointer));
-                        if (j % 2 == 0) pointer += add1;
-                        else pointer += add2;
-                        j++;
-                    }
-                    else break;
-                }
-                add1 -= 2;
-                add2 += 2;
+            if (row == nRows - 1){
+                head = -1;
             }
+            sbRow[row].append(s.charAt(i));
+            row += head;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nRows; i++){
+            sb.append(sbRow[i].toString());
         }
         return sb.toString();
     }
