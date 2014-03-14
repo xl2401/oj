65b240e7fefa04d6c4f12708fb880ca98fdb1daf
Yuhan Zhang <njuprincerain@gmail.com>
2014-03-14 10:51:03 -0400

    iterative subset2

--- Subset2.java
+++ Subset2.java
@@ -2,54 +2,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Subset2 {
-    private ArrayList<ArrayList<Integer>> lastLayer = new ArrayList<ArrayList<Integer>>();
-
     public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] S) {
         Arrays.sort(S);
-        return subsets(S, S.length-1);
-    }
-
-    public ArrayList<ArrayList<Integer>> subsets(int[] S, int idx){
-        if (idx == -1){
-            ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-            ArrayList<Integer> emptySet = new ArrayList<Integer>();
-            result.add(emptySet);
-            return result;
-        }
-        else{
-            ArrayList<ArrayList<Integer>> lastResult = subsets(S, idx-1);
-            ArrayList<ArrayList<Integer>> curLayer = new ArrayList<ArrayList<Integer>>();
-            ArrayList<ArrayList<Integer>> prev = lastResult;
-            if (idx != 0 && S[idx] == S[idx-1]){
-                prev = this.lastLayer;
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> emptySet = new ArrayList<Integer>();
+        res.add(emptySet);
+        int N = S.length;
+        int start = 0;
+        for (int i = 0; i < N; i++){
+            int size = res.size();
+            for (int j = start; j < size; j++){
+                ArrayList<Integer> curSet = new ArrayList<Integer>(res.get(j));
+                curSet.add(S[i]);
+                res.add(curSet);
             }
-            for (ArrayList<Integer> set: prev){
-                ArrayList<Integer> newSet = copyList(set);
-                newSet.add(S[idx]);
-                curLayer.add(newSet);
+            if (i < N-1 && S[i] == S[i+1]){
+                start = size;
             }
-            this.lastLayer = curLayer;
-            lastResult.addAll(curLayer);
-            return lastResult;
-        }
-    }
-
-    public ArrayList<Integer> copyList(ArrayList<Integer> l){
-        ArrayList<Integer> newL = new ArrayList<Integer>();
-        for (Integer i: l){
-            newL.add(i);
-        }
-        return newL;
-    }
-
-    public static void main(String[] args){
-        int[] A = {1,1,2,2,2};
-        ArrayList<ArrayList<Integer>> result = new Subset2().subsetsWithDup(A);
-        for (ArrayList<Integer> set: result){
-            for (Integer i: set){
-                System.out.print(i + " ");
+            else{
+                start = 0;
             }
-            System.out.println();
         }
+        return res;
     }
 }
