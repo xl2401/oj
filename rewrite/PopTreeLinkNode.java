27f703406bced8f82114800a7218153107c5f981
Yuhan Zhang <njuprincerain@gmail.com>
2014-07-27 16:07:00 -0400

    [rewrite] pop BT next right node

---
+++ rewrite/PopTreeLinkNode.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+
+public class Solution {
+    public void connect(TreeLinkNode root) {  
+    	// extreme null check
+    	if (root == null) return;
+    	LinkedList<TreeLinkNode> lastLayer = new LinkedList<TreeLinkNode>();
+    	lastLayer.add(root);
+    	while (!lastLayer.isEmpty()) {
+    		LinkedList<TreeLinkNode> curLayer = new LinkedList<TreeLinkNode>();
+    		TreeLinkNode lastNode = null;
+    		for (TreeLinkNode node: lastLayer) {
+    			if (node.left != null)
+    				curLayer.add(node.left);
+    			if (node.right != null)
+    				curLayer.add(node.right);
+    		}
+    		connectList(curLayer);
+    		lastLayer = curLayer;
+    	}
+    }
+    
+    private void connectList(LinkedList<TreeLinkNode> list) {
+    	// dummy node
+    	TreeLinkNode prev = new TreeLinkNode(0);
+    	for (TreeLinkNode node: list) {
+    		prev.next = node;
+    		prev = node;
+    	}
+    }
+}
